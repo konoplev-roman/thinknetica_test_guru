94091cd82494d5e1c6789d970fb0d42bb1c21cfa
Roman Konoplev <roman@konoplev.pro>
2019-07-02 22:51:35 +0300

    Add questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_02_194654) do
+ActiveRecord::Schema.define(version: 2019_07_02_194954) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
+    t.text "body"
+    t.bigint "test_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
@@ -26,5 +34,6 @@ ActiveRecord::Schema.define(version: 2019_07_02_194654) do
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
+  add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
 end
