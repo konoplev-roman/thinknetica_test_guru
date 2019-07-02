bc78e9aff3363e7400574a1a8e0467c98df8ec7a
Roman Konoplev <roman@konoplev.pro>
2019-07-02 23:02:01 +0300

    Set the Category title not null

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_02_195412) do
+ActiveRecord::Schema.define(version: 2019_07_02_195939) do
 
   create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
     t.text "body"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2019_07_02_195412) do
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
