33d98158c21c53bd3c5c580158cbf628d4c9ebaf
Roman Konoplev <roman@konoplev.pro>
2019-07-02 23:12:55 +0300

    Add test level with a default value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_02_200802) do
+ActiveRecord::Schema.define(version: 2019_07_02_201123) do
 
   create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
     t.text "body", null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_07_02_200802) do
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "level", default: 0
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
