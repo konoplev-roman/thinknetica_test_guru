e3cdd5e8181824d5e73f75ed900f2e06b63b37e5
Roman Konoplev <roman@konoplev.pro>
2019-07-02 23:08:36 +0300

    Set the User name not null

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_02_200641) do
+ActiveRecord::Schema.define(version: 2019_07_02_200802) do
 
   create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
     t.text "body", null: false
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2019_07_02_200641) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
