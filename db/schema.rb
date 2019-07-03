2f92ae7b23ac78959c7a4f48c4425af859209f86
Roman Konoplev <roman@konoplev.pro>
2019-07-04 00:51:51 +0300

    Add tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_03_214758) do
+ActiveRecord::Schema.define(version: 2019_07_03_215031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,14 @@ ActiveRecord::Schema.define(version: 2019_07_03_214758) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tests", force: :cascade do |t|
+    t.bigint "category_id"
+    t.string "title"
+    t.integer "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_tests_on_category_id"
+  end
+
+  add_foreign_key "tests", "categories"
 end
