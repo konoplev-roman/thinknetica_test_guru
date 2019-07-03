80dbf76e48cfd7069132ba313cf5e2a5ce20c3ea
Roman Konoplev <roman@konoplev.pro>
2019-07-04 01:08:40 +0300

    Set the User name not null

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_03_220654) do
+ActiveRecord::Schema.define(version: 2019_07_03_220814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 2019_07_03_220654) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
