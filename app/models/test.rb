4dadce9ee3c7bc093cd9d960e320bc0aa0fbb601
Roman Konoplev <roman@konoplev.pro>
2019-07-05 14:22:15 +0300

    Refactor getting method sorted test names

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,7 @@
 class Test < ApplicationRecord
   def self.names_by_category(category)
-    Test
-      .joins('JOIN categories ON tests.category_id = categories.id')
-      .where('categories.title' => category)
+    joins('JOIN categories ON tests.category_id = categories.id')
+      .where(categories: { title: category })
       .order('tests.title DESC')
       .pluck('tests.title')
   end
