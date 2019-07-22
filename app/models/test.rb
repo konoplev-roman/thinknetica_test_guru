4926ccd32435a486b8a1441c625865ce61f80548
Roman Konoplev <roman@konoplev.pro>
2019-07-22 20:17:32 +0300

    Add a аssociation of tests to category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,11 @@
 class Test < ApplicationRecord
+  belongs_to :category
   belongs_to :author, class_name: 'User'
   has_many :passed_tests
   has_many :users, through: :passed_tests
 
   def self.names_by_category(category)
-    joins('JOIN categories ON tests.category_id = categories.id')
+    joins(:category)
       .where(categories: { title: category })
       .order('tests.title DESC')
       .pluck('tests.title')
