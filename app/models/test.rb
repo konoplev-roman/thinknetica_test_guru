96e3ded8494e39504fc423b51525e954fad9c07f
Roman Konoplev <roman@konoplev.pro>
2019-07-22 20:06:14 +0300

    Add a аssociation of passed tests to users

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  has_many :passed_tests
+  has_many :users, through: :passed_tests
 
   def self.names_by_category(category)
     joins('JOIN categories ON tests.category_id = categories.id')
