4c08765ef56d85986a689b5a632ad5db1437b793
Roman Konoplev <roman@konoplev.pro>
2019-07-23 23:52:14 +0300

    Add name and email validation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :passed_tests, dependent: :destroy
   has_many :tests, through: :passed_tests
 
+  validates :name, presence: true
+  validates :email, presence: true
+
   def tests_by_level(level)
     tests.level(level)
   end
