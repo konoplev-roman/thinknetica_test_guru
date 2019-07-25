76c7d36b42845ea3cf222ba13edcdbf208f31b72
Roman Konoplev <roman@konoplev.pro>
2019-07-25 10:05:21 +0300

    Refactor validations of answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,13 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :question, :body, presence: true
-  validate :validate_count_answers
+  validate :validate_count_answers, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   private
 
   def validate_count_answers
-    errors.add(:base) if question.answers.size > 3
+    errors.add(:base) if question.answers.count >= 4
   end
 end
