dbd6f1c1d67067c055d9a044643c9fee1fce5333
Roman Konoplev <roman@konoplev.pro>
2019-07-26 22:35:42 +0300

    Add exception handling when record not found

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,8 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index]
 
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_not_found
+
   def index
     result = @test.questions.pluck(:body)
 
@@ -11,5 +13,9 @@ class QuestionsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def rescue_with_not_found
+    render plain: 'Record was not found!!!'
   end
 end
