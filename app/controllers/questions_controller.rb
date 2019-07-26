74ea4c286954190ad3b4509378ab49b6fd1b83f3
Roman Konoplev <roman@konoplev.pro>
2019-07-26 22:32:36 +0300

    Refactor questions#index. Add before_action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,15 @@
 class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index]
+
   def index
-    result = Test.find(params[:test_id]).questions.pluck(:body)
+    result = @test.questions.pluck(:body)
 
     render plain: result.join("\n")
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
   end
 end
