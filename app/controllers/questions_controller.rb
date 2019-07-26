eeab0581ad003bc2410bea25dd37c49c3e461248
Roman Konoplev <roman@konoplev.pro>
2019-07-26 22:38:11 +0300

    Add questions#show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index]
+  before_action :find_question, only: %i[show]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_not_found
 
@@ -9,10 +10,18 @@ class QuestionsController < ApplicationController
     render plain: result.join("\n")
   end
 
+  def show
+    render plain: @question.body
+  end
+
   private
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def rescue_with_not_found
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :tests, only: [] do
-    resources :questions, only: %i[index], shallow: true
+    resources :questions, only: %i[index show], shallow: true
   end
 end
