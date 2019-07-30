79b733c1a7bac3d3b2f014f70e50dd6b5dba0523
Roman Konoplev <roman@konoplev.pro>
2019-07-31 00:52:14 +0300

    Add index page with list of tests. Temporarily add stubs instead of links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :tests, only: [] do
+  resources :tests, only: %i[index] do
     resources :questions, except: %i[edit update], shallow: true
   end
 end
