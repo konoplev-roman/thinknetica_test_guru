e8ff0733ba2c9854eefebcd4f37a4be5d01913d6
Roman Konoplev <roman@konoplev.pro>
2019-07-30 23:30:37 +0300

    Add a helper that returns a link to the Github repository

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def year
     Date.today.year
   end
+
+  def github_url(author, repo)
+    link_to 'GitHub', "https://github.com/#{author}/#{repo}", target: '_blank'
+  end
 end
