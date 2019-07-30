3c761fb258b89e9b31f91143d943ae6df0e79914
Roman Konoplev <roman@konoplev.pro>
2019-07-31 01:06:20 +0300

    Add tests#destroy

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,19 @@
 class TestsController < ApplicationController
+  before_action :find_test, only: %i[destroy]
+
   def index
     @tests = Test.all
+  end
+
+  def destroy
+    @test.destroy
+
+    redirect_to tests_path
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
   end
 end
