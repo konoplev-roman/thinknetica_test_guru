b4f8d890bd53406c416b1ee4b2e15f02701b3c6a
Roman Konoplev <roman@konoplev.pro>
2019-07-31 21:43:35 +0300

    Add actions for creating and editing tests. Temporarily add a random author for tests

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,5 @@
 class TestsController < ApplicationController
-  before_action :find_test, only: %i[show destroy]
+  before_action :find_test, only: %i[show destroy edit update]
 
   def index
     @tests = Test.all
@@ -13,9 +13,43 @@ class TestsController < ApplicationController
     redirect_to tests_path
   end
 
+  def new
+    @test = Test.new
+
+    render '_form'
+  end
+
+  def create
+    @test = Test.new(test_params)
+
+    @test.author = User.all.sample
+
+    if @test.save
+      redirect_to test_path(@test)
+    else
+      render '_form'
+    end
+  end
+
+  def edit
+    render '_form'
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to test_path(@test)
+    else
+      render '_form'
+    end
+  end
+
   private
 
   def find_test
     @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :category_id, :level)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'tests#index'
 
-  resources :tests, only: %i[index destroy show] do
+  resources :tests do
     resources :questions, except: %i[index], shallow: true
   end
 end
