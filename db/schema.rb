56fbcb988587eef85886b1bc4f4f9dbb6d44640a
Roman Konoplev <roman@konoplev.pro>
2019-08-15 19:34:27 +0300

    Add non unique index to PassedTests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_15_162947) do
+ActiveRecord::Schema.define(version: 2019_08_15_163123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_08_15_162947) do
     t.bigint "question_id"
     t.index ["question_id"], name: "index_passed_tests_on_question_id"
     t.index ["test_id"], name: "index_passed_tests_on_test_id"
+    t.index ["user_id", "test_id"], name: "index_passed_tests_on_user_id_and_test_id"
     t.index ["user_id"], name: "index_passed_tests_on_user_id"
   end
 
