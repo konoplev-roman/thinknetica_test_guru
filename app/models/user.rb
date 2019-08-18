a17e520ecbf907c425bbe4a9b5a658be0007d671
Roman Konoplev <roman@konoplev.pro>
2019-08-18 19:40:39 +0300

    Refactor user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,11 @@
-require 'digest/sha1'
-
 class User < ApplicationRecord
-  attr_reader :password
-  attr_writer :password_confirmation
+  include Auth
 
   has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
   has_many :passed_tests, dependent: :destroy
   has_many :tests, through: :passed_tests
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, if: proc { |u| u.password_digest.blank? }
-  validates :password, confirmation: true
 
   def tests_by_level(level)
     tests.level(level)
@@ -19,25 +13,5 @@ class User < ApplicationRecord
 
   def passed_test(test)
     passed_tests.order(id: :desc).find_by(test_id: test.id)
-  end
-
-  def authenticate(password_string)
-    digest(password_string) == password_digest ? self : false
-  end
-
-  def password=(password_string)
-    if password_string.nil?
-      self.password_digest = nil
-    elsif password_string.present?
-      @password = password_string
-
-      self.password_digest = digest(password_string)
-    end
-  end
-
-  private
-
-  def digest(string)
-    Digest::SHA1.hexdigest(string)
   end
 end
