d085c523b19695d64939fa7d32cea8a7e784c761
Roman Konoplev <roman@konoplev.pro>
2019-08-19 00:35:05 +0300

    Add the helper with the output flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to 'GitHub', "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
+  def flash_class(level)
+    case level.to_sym
+    when :notice then 'alert alert-info'
+    when :success then 'alert alert-success'
+    when :error then 'alert alert-danger'
+    when :alert then 'alert alert-danger'
+    end
+  end
 end
