a596e6dab42ddfbfd918fbb8a5750248019aae27
Roman Konoplev <roman@konoplev.pro>
2019-08-21 17:57:22 +0300

    Refactor the devise controller and custom greeting after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,8 @@
 class SessionsController < Devise::SessionsController
   # POST /resource/sign_in
   def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in, name: resource.first_name)
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
+    super
+
+    flash.notice = "Welcome, #{current_user.first_name} Guru!"
   end
 end
