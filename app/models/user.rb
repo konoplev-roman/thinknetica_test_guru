e994b1df536de47b17343ce87120b0732d194645
Roman Konoplev <roman@konoplev.pro>
2019-08-21 10:50:21 +0300

    Refactor the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
+  has_many :passed_tests, dependent: :destroy
+  has_many :tests, through: :passed_tests
+
+  validates :name, presence: true
+
   devise :database_authenticatable,
          :registerable,
          :recoverable,
@@ -6,12 +12,6 @@ class User < ApplicationRecord
          :trackable,
          :validatable,
          :confirmable
-
-  has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
-  has_many :passed_tests, dependent: :destroy
-  has_many :tests, through: :passed_tests
-
-  validates :name, presence: true
 
   def tests_by_level(level)
     tests.level(level)
