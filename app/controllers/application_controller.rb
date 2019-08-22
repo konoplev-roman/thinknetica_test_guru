b6defe1c0c66acf14c3c7902ea322392441fc89b
Roman Konoplev <roman@konoplev.pro>
2019-08-22 17:25:42 +0300

    Add lang parameter to url if different from default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def default_url_options
+    { lang: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
 
   protected
 
@@ -11,5 +16,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(user)
     user.admin? ? admin_tests_path : tests_path
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
