f950c80a34232eef8be470703896f2a88a43b67f
konoplev-roman <roman@konoplev.pro>
2020-01-19 04:52:59 +0300

    Change subject of the test completion email from the localization files

--- app/mailers/tests_mailer.rb
+++ app/mailers/tests_mailer.rb
@@ -3,6 +3,6 @@ class TestsMailer < ApplicationMailer
     @user = passed_test.user
     @test = passed_test.test
 
-    mail to: @user.email, subject: 'You just completed the TestGuru test!'
+    mail to: @user.email, subject: t('tests_mailer.completed_test.subject')
   end
 end
