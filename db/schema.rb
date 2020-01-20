a3ca2924a6a8f1c839de70b9c39ded176793de8c
konoplev-roman <roman@konoplev.pro>
2020-01-20 21:18:58 +0300

    Add new fields to use the timer in tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_192127) do
+ActiveRecord::Schema.define(version: 2020_01_20_181410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,8 @@ ActiveRecord::Schema.define(version: 2019_08_25_192127) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
+    t.boolean "has_timer", default: false, null: false
+    t.integer "time_limit"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
     t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
