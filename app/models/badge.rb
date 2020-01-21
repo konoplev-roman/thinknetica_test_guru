f03498fe26b6c32ce6201a2b42dd463343b5a8e6
konoplev-roman <roman@konoplev.pro>
2020-01-21 23:57:49 +0300

    Add model Achievement

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,2 +1,4 @@
 class Badge < ApplicationRecord
+  has_many :achievements, dependent: :destroy
+  has_many :users, through: :achievements
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_204023) do
+ActiveRecord::Schema.define(version: 2020_01_21_205225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "achievements", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "badge_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["badge_id"], name: "index_achievements_on_badge_id"
+    t.index ["user_id"], name: "index_achievements_on_user_id"
+  end
 
   create_table "answers", force: :cascade do |t|
     t.bigint "question_id", null: false
@@ -109,6 +118,8 @@ ActiveRecord::Schema.define(version: 2020_01_21_204023) do
     t.index ["type"], name: "index_users_on_type"
   end
 
+  add_foreign_key "achievements", "badges"
+  add_foreign_key "achievements", "users"
   add_foreign_key "answers", "questions"
   add_foreign_key "gists", "questions"
   add_foreign_key "gists", "users"
