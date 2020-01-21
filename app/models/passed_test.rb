dcf7ab938ffffb581bf2240b5be959efe394f28f
konoplev-roman <roman@konoplev.pro>
2020-01-21 22:04:10 +0300

    Refactor: private methods

--- app/models/passed_test.rb
+++ app/models/passed_test.rb
@@ -43,15 +43,6 @@ class PassedTest < ApplicationRecord
     created_at + test.time_limit.minutes
   end
 
-  def time_expired?
-    # If the timer is not set the pass test time never expires
-    timer? && Time.now > complete_before
-  end
-
-  def time_left?
-    !time_expired?
-  end
-
   private
 
   def before_validation_set_question
@@ -76,5 +67,14 @@ class PassedTest < ApplicationRecord
 
   def current_question?
     !!current_question
+  end
+
+  def time_expired?
+    # If the timer is not set the pass test time never expires
+    timer? && Time.now > complete_before
+  end
+
+  def time_left?
+    !time_expired?
   end
 end
