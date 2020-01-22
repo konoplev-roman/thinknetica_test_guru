7606df7bac427105d7ef611e509e11ebee6284d3
konoplev-roman <roman@konoplev.pro>
2020-01-22 14:27:44 +0300

    Add target to Badge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_205225) do
+ActiveRecord::Schema.define(version: 2020_01_22_112634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_01_21_205225) do
     t.string "condition", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "target"
   end
 
   create_table "categories", force: :cascade do |t|
