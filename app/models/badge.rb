9105430bb14485dd607186c4df54a2f139164aeb
konoplev-roman <roman@konoplev.pro>
2020-01-22 15:18:25 +0300

    Add validates to Badge

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,4 +1,10 @@
 class Badge < ApplicationRecord
   has_many :achievements, dependent: :destroy
   has_many :users, through: :achievements
+
+  validates :title, :condition, :image, presence: true
+  validates :target, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
+  validates :condition, uniqueness: { scope: :target }
+
+  scope :target, ->(target) { where(target: target) }
 end
