8725e2c86e5f31bd8fcdd3e1d53d0d301501fc66
konoplev-roman <roman@konoplev.pro>
2020-01-23 22:33:00 +0300

    Refactor: change the validation of target for the condition
    Some conditions don't have target.
    For example, getting a badge for receiving all other achievements, or for absolutely all passed tests.
    In such cases, the badge must be saved with a null value in the "target" field.
    Otherwise, the last value selected on the form will be saved in this field.
    This may negatively affect the retention of the "condition+target" index.
    
    Splitting an array CONDITIONS into two is a temporary solution.
    In the future, method need_target? it will be implemented better.

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -3,8 +3,13 @@ class Badge < ApplicationRecord
   has_many :users, through: :achievements
 
   validates :title, :condition, :image, presence: true
-  validates :target, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
+  validates :target, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true, if: :need_target?
+  validates :target, numericality: { only_integer: true, greater_than: 0 }, allow_nil: false, unless: :need_target?
   validates :condition, uniqueness: { scope: :target }, inclusion: { in: AchievementService::CONDITIONS }
 
   scope :target, ->(target) { where(target: target) }
+
+  def need_target?
+    AchievementService::CONDITIONS_WITHOUT_TARGET.include?(condition)
+  end
 end
--- app/services/achievement_service.rb
+++ app/services/achievement_service.rb
@@ -1,9 +1,14 @@
 class AchievementService
-  CONDITIONS = %w[
+  CONDITIONS_WITH_TARGET = %w[
     success_by_category
     success_by_level
     success_first_attempt
     without_errors
+  ].freeze
+
+  CONDITIONS_WITHOUT_TARGET = %w[
     all_achievements
   ].freeze
+
+  CONDITIONS = CONDITIONS_WITH_TARGET + CONDITIONS_WITHOUT_TARGET
 end
